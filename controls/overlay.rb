9bf128ed4206c0bdec613ef8c6a2cf1219c9f217
Krishna Kola <kkola@digitalinfuzion.com>
2019-12-03 12:49:03 -0500

    add additional files

---
+++ controls/overlay.rb
@@ -0,0 +1,818 @@
+include_controls 'microsoft-windows-server-2016-stig-baseline' do
+  control 'V-73699' do
+
+  end
+  control 'V-73419' do
+
+  end
+  control 'V-73559' do
+
+  end
+  control 'V-73217' do
+
+  end
+  control 'V-73223' do
+
+  end
+  control 'V-73363' do
+
+  end
+  control 'V-73281' do
+
+  end
+  control 'V-73291' do
+
+  end
+  control 'V-73373' do
+
+  end
+  control 'V-73233' do
+
+  end
+  control 'V-73549' do
+
+  end
+  control 'V-73409' do
+
+  end
+  control 'V-73569' do
+
+  end
+  control 'V-73429' do
+
+  end
+  control 'V-73227' do
+
+  end
+  control 'V-73367' do
+
+  end
+  control 'V-73285' do
+
+  end
+  control 'V-73295' do
+
+  end
+  control 'V-73377' do
+
+  end
+  control 'V-73237' do
+
+  end
+  control 'V-73439' do
+
+  end
+  control 'V-73579' do
+
+  end
+  control 'V-73809' do
+
+  end
+  control 'V-73539' do
+
+  end
+  control 'V-73479' do
+
+  end
+  control 'V-73277' do
+
+  end
+  control 'V-73395' do
+
+  end
+  control 'V-73303' do
+
+  end
+  control 'V-90359' do
+
+  end
+  control 'V-73253' do
+
+  end
+  control 'V-73313' do
+
+  end
+  control 'V-73385' do
+
+  end
+  control 'V-73267' do
+
+  end
+  control 'V-73469' do
+
+  end
+  control 'V-73529' do
+
+  end
+  control 'V-73449' do
+
+  end
+  control 'V-73509' do
+
+  end
+  control 'V-73789' do
+
+  end
+  control 'V-73307' do
+
+  end
+  control 'V-73247' do
+
+  end
+  control 'V-73273' do
+
+  end
+  control 'V-73391' do
+
+  end
+  control 'V-73381' do
+
+  end
+  control 'V-73323' do
+
+  end
+  control 'V-73263' do
+
+  end
+  control 'V-73257' do
+
+  end
+  control 'V-73317' do
+
+  end
+  control 'V-73799' do
+
+  end
+  control 'V-73459' do
+
+  end
+  control 'V-73229' do
+
+  end
+  control 'V-73369' do
+
+  end
+  control 'V-73567' do
+
+  end
+  control 'V-73427' do
+
+  end
+  control 'V-73605' do
+
+  end
+  control 'V-73745' do
+
+  end
+  control 'V-73693' do
+
+  end
+  control 'V-73413' do
+
+  end
+  control 'V-73553' do
+
+  end
+  control 'V-73771' do
+
+  end
+  control 'V-73631' do
+
+  end
+  control 'V-73621' do
+
+  end
+  control 'V-73761' do
+
+  end
+  control 'V-73543' do
+
+  end
+  control 'V-73403' do
+
+  end
+  control 'V-73683' do
+
+  end
+  control 'V-73755' do
+
+  end
+  control 'V-73615' do
+
+  end
+  control 'V-73807' do
+
+  end
+  control 'V-73495' do
+
+  end
+  control 'V-73437' do
+
+  end
+  control 'V-73577' do
+
+  end
+  control 'V-73379' do
+
+  end
+  control 'V-73239' do
+
+  end
+  control 'V-73359' do
+
+  end
+  control 'V-73219' do
+
+  end
+  control 'V-73417' do
+
+  end
+  control 'V-73557' do
+
+  end
+  control 'V-73697' do
+
+  end
+  control 'V-73775' do
+
+  end
+  control 'V-73635' do
+
+  end
+  control 'V-73563' do
+
+  end
+  control 'V-73423' do
+
+  end
+  control 'V-73601' do
+
+  end
+  control 'V-73741' do
+
+  end
+  control 'V-73481' do
+
+  end
+  control 'V-73491' do
+
+  end
+  control 'V-73803' do
+
+  end
+  control 'V-73751' do
+
+  end
+  control 'V-73611' do
+
+  end
+  control 'V-73433' do
+
+  end
+  control 'V-73573' do
+
+  end
+  control 'V-73625' do
+
+  end
+  control 'V-73765' do
+
+  end
+  control 'V-73687' do
+
+  end
+  control 'V-73547' do
+
+  end
+  control 'V-73407' do
+
+  end
+  control 'V-73309' do
+
+  end
+  control 'V-73249' do
+
+  end
+  control 'V-73787' do
+
+  end
+  control 'V-73447' do
+
+  end
+  control 'V-73507' do
+
+  end
+  control 'V-73665' do
+
+  end
+  control 'V-73533' do
+
+  end
+  control 'V-73473' do
+
+  end
+  control 'V-73591' do
+
+  end
+  control 'V-73651' do
+
+  end
+  control 'V-73711' do
+
+  end
+  control 'V-73701' do
+
+  end
+  control 'V-73641' do
+
+  end
+  control 'V-78127' do
+
+  end
+  control 'V-73581' do
+
+  end
+  control 'V-73463' do
+
+  end
+  control 'V-73675' do
+
+  end
+  control 'V-73735' do
+
+  end
+  control 'V-73457' do
+
+  end
+  control 'V-73797' do
+
+  end
+  control 'V-73259' do
+
+  end
+  control 'V-73319' do
+
+  end
+  control 'V-73279' do
+
+  end
+  control 'V-73537' do
+
+  end
+  control 'V-73477' do
+
+  end
+  control 'V-73655' do
+
+  end
+  control 'V-73715' do
+
+  end
+  control 'V-73595' do
+
+  end
+  control 'V-73443' do
+
+  end
+  control 'V-73503' do
+
+  end
+  control 'V-73783' do
+
+  end
+  control 'V-73721' do
+
+  end
+  control 'V-73661' do
+
+  end
+  control 'V-73731' do
+
+  end
+  control 'V-73793' do
+
+  end
+  control 'V-73513' do
+
+  end
+  control 'V-73453' do
+
+  end
+  control 'V-78123' do
+
+  end
+  control 'V-73585' do
+
+  end
+  control 'V-73705' do
+
+  end
+  control 'V-73645' do
+
+  end
+  control 'V-73467' do
+
+  end
+  control 'V-73527' do
+
+  end
+  control 'V-90357' do
+
+  end
+  control 'V-73777' do
+
+  end
+  control 'V-73637' do
+
+  end
+  control 'V-73695' do
+
+  end
+  control 'V-73415' do
+
+  end
+  control 'V-73555' do
+
+  end
+  control 'V-73483' do
+
+  end
+  control 'V-73603' do
+
+  end
+  control 'V-73743' do
+
+  end
+  control 'V-73561' do
+
+  end
+  control 'V-73431' do
+
+  end
+  control 'V-73571' do
+
+  end
+  control 'V-73753' do
+
+  end
+  control 'V-73613' do
+
+  end
+  control 'V-73801' do
+
+  end
+  control 'V-73493' do
+
+  end
+  control 'V-73545' do
+
+  end
+  control 'V-73405' do
+
+  end
+  control 'V-73685' do
+
+  end
+  control 'V-73627' do
+
+  end
+  control 'V-73767' do
+
+  end
+  control 'V-73289' do
+
+  end
+  control 'V-73607' do
+
+  end
+  control 'V-73747' do
+
+  end
+  control 'V-73487' do
+
+  end
+  control 'V-73565' do
+
+  end
+  control 'V-73773' do
+
+  end
+  control 'V-73633' do
+
+  end
+  control 'V-73411' do
+
+  end
+  control 'V-73551' do
+
+  end
+  control 'V-73691' do
+
+  end
+  control 'V-73681' do
+
+  end
+  control 'V-73541' do
+
+  end
+  control 'V-73401' do
+
+  end
+  control 'V-73623' do
+
+  end
+  control 'V-73763' do
+
+  end
+  control 'V-73435' do
+
+  end
+  control 'V-73575' do
+
+  end
+  control 'V-73497' do
+
+  end
+  control 'V-73757' do
+
+  end
+  control 'V-73617' do
+
+  end
+  control 'V-73299' do
+
+  end
+  control 'V-73399' do
+
+  end
+  control 'V-73597' do
+
+  end
+  control 'V-73657' do
+
+  end
+  control 'V-73717' do
+
+  end
+  control 'V-73475' do
+
+  end
+  control 'V-73663' do
+
+  end
+  control 'V-73781' do
+
+  end
+  control 'V-73441' do
+
+  end
+  control 'V-73501' do
+
+  end
+  control 'V-73511' do
+
+  end
+  control 'V-73451' do
+
+  end
+  control 'V-73791' do
+
+  end
+  control 'V-73673' do
+
+  end
+  control 'V-73733' do
+
+  end
+  control 'V-73465' do
+
+  end
+  control 'V-73525' do
+
+  end
+  control 'V-73707' do
+
+  end
+  control 'V-73647' do
+
+  end
+  control 'V-73587' do
+
+  end
+  control 'V-73389' do
+
+  end
+  control 'V-90361' do
+
+  end
+  control 'V-90355' do
+
+  end
+  control 'V-73727' do
+
+  end
+  control 'V-73667' do
+
+  end
+  control 'V-73445' do
+
+  end
+  control 'V-73505' do
+
+  end
+  control 'V-73785' do
+
+  end
+  control 'V-73653' do
+
+  end
+  control 'V-73713' do
+
+  end
+  control 'V-73593' do
+
+  end
+  control 'V-73531' do
+
+  end
+  control 'V-73471' do
+
+  end
+  control 'V-73461' do
+
+  end
+  control 'V-73521' do
+
+  end
+  control 'V-78125' do
+
+  end
+  control 'V-73583' do
+
+  end
+  control 'V-73643' do
+
+  end
+  control 'V-73795' do
+
+  end
+  control 'V-73515' do
+
+  end
+  control 'V-73455' do
+
+  end
+  control 'V-73677' do
+
+  end
+  control 'V-73737' do
+
+  end
+  control 'V-73489' do
+
+  end
+  control 'V-73609' do
+
+  end
+  control 'V-73749' do
+
+  end
+  control 'V-73287' do
+
+  end
+  control 'V-73225' do
+
+  end
+  control 'V-73365' do
+
+  end
+  control 'V-73375' do
+
+  end
+  control 'V-73235' do
+
+  end
+  control 'V-73297' do
+
+  end
+  control 'V-73759' do
+
+  end
+  control 'V-73499' do
+
+  end
+  control 'V-73779' do
+
+  end
+  control 'V-73639' do
+
+  end
+  control 'V-73283' do
+
+  end
+  control 'V-73221' do
+
+  end
+  control 'V-73361' do
+
+  end
+  control 'V-73371' do
+
+  end
+  control 'V-73231' do
+
+  end
+  control 'V-73293' do
+
+  end
+  control 'V-73629' do
+
+  end
+  control 'V-73769' do
+
+  end
+  control 'V-73729' do
+
+  end
+  control 'V-73669' do
+
+  end
+  control 'V-73305' do
+
+  end
+  control 'V-73245' do
+
+  end
+  control 'V-73393' do
+
+  end
+  control 'V-73271' do
+
+  end
+  control 'V-73321' do
+
+  end
+  control 'V-73261' do
+
+  end
+  control 'V-73383' do
+
+  end
+  control 'V-73255' do
+
+  end
+  control 'V-73315' do
+
+  end
+  control 'V-73679' do
+
+  end
+  control 'V-73739' do
+
+  end
+  control 'V-73719' do
+
+  end
+  control 'V-73599' do
+
+  end
+  control 'V-73397' do
+
+  end
+  control 'V-73275' do
+
+  end
+  control 'V-73301' do
+
+  end
+  control 'V-73241' do
+
+  end
+  control 'V-73251' do
+
+  end
+  control 'V-73311' do
+
+  end
+  control 'V-73325' do
+
+  end
+  control 'V-73265' do
+
+  end
+  control 'V-73387' do
+
+  end
+  control 'V-73589' do
+
+  end
+  control 'V-73709' do
+
+  end
+  control 'V-73649' do
+
+  end
+  control 'V-91779' do
+
+  end
+end
